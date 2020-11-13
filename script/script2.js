d52e0392ac3e671fe3eaa3dfaaaa0020aa6bd03a
it <oleg-ivannikov@mail.ru>
2020-11-13 17:43:47 +0300

    JS лекция №2

---
+++ script/script2.js
@@ -0,0 +1,78 @@
+//Задача №1
+/*
+let month = Number(prompt("Введите номер месяца"));
+
+switch ( month ) {
+	case 2:
+	case 3:
+	case 4:
+		alert("Весна");
+		break;
+	case 5:
+	case 6:
+	case 7:
+		alert("Лето");
+		break;
+	case 8:
+	case 9:
+	case 10:
+		alert("Осень");
+		break;
+	case 1:
+	case 11:
+	case 12:
+		alert("Зима");
+		break;
+	default:
+		alert("Вы ввели не число месяца!");
+}
+*/
+//Задача №2
+/*
+let lengthType = +prompt("Введите единицу длины." + 
+						 "1 — дециметр, 2 — километр, 3 — метр, 4 — миллиметр, 5 — сантиметр", 3);
+let L = +prompt("Введите длину", 100);
+
+switch ( lengthType ) {
+	//дециметр
+	case 1:
+		alert(L * 0.1 + " метров");
+		break;
+	//километр
+	case 2:
+		alert(L * 10 + " метров");
+		break;
+	//метр
+	case 3:
+		alert( L + " метров");
+		break;
+	//миллиметр
+	case 4:
+		alert(L * 0.001 + " метров");
+		break;
+	//сантиметр
+	case 5:
+		alert(L * 0.01 + " метров");
+		break;
+	default :
+		alert("Вы ввели неверную единицу измерения!");
+}
+*/
+
+//Задача №3
+
+let num = +prompt("", 555);
+let message = "";
+
+switch ( ( num >= -999 ) && ( num <= 999) {
+	case ( num < 0 ):
+		message += "";
+		break;
+	case ( num > 0 ):
+		message += "";
+		break;
+	case ( num ;
+	case;
+	case;
+	default:
+}
