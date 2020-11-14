382ce97b2f2c4e226131f05e6ac2cd94f3bbd1f8
oleg victorovich <oleg-ivannikov@mail.ru>
2020-11-14 15:21:44 +0300

    JS Homework #2

--- script/script2.js
+++ script/script2.js
@@ -1,5 +1,5 @@
 //Задача №1
-/*
+
 let month = Number(prompt("Введите номер месяца"));
 
 switch ( month ) {
@@ -26,9 +26,9 @@ switch ( month ) {
 	default:
 		alert("Вы ввели не число месяца!");
 }
-*/
+
 //Задача №2
-/*
+
 let lengthType = +prompt("Введите единицу длины." + 
 						 "1 — дециметр, 2 — километр, 3 — метр, 4 — миллиметр, 5 — сантиметр", 3);
 let L = +prompt("Введите длину", 100);
@@ -40,7 +40,7 @@ switch ( lengthType ) {
 		break;
 	//километр
 	case 2:
-		alert(L * 10 + " метров");
+		alert(L * 1000 + " метров");
 		break;
 	//метр
 	case 3:
@@ -57,22 +57,78 @@ switch ( lengthType ) {
 	default :
 		alert("Вы ввели неверную единицу измерения!");
 }
-*/
+
 
 //Задача №3
 
-let num = +prompt("", 555);
+let num = +prompt("Введите число  в диапазоне от -999 до 999", 555);
 let message = "";
-
-if( num > 0 {
-	message += "";	
+const lowRange = -999, highRange = 999;
+// положительное или отрицательное
+if( num > 0 ) {
+	message += "положительное ";
 } else if ( num < 0 ) {
-		message += "";
-	} else {
-		message += "";
-	}
+	message += "отрицательное ";
+} else {
+
 }
-	
-if( num >= 100 ) {
-	message += "";
-} else if (
+// разряд числа
+if ( Math.abs( num ) >= 100 ) {
+	message += "трехзначное ";
+} else if ( Math.abs( num ) >= 10 ) {
+	message += "двузначное ";
+} else {
+	message += "однозначное ";
+}
+
+message += "число";
+// нулевое число
+if ( num == 0 ) {
+	message = "нулевое число";
+}
+
+console.log(message);
+
+
+//Задача №4
+
+for (let i = 1 ; i <=100; i++){
+	if( !( i % 3 ) && !( i % 5 ) ){
+		console.log("ThreeFive");
+		continue;
+	}
+	if ( !( i % 3 ) ) {
+		console.log("Three");
+		continue;
+	}
+	if ( !( i % 5 ) ) {
+		console.log("Five");
+		continue;
+	}
+	console.log(i);
+ }
+ 
+ //Задача №5
+ 
+ let year = prompt("Введите год", 2020);
+ 
+ if (  year % 4  ) {
+	 console.log("Не высокосный год");
+ } else if ( year % 400 )  {
+	 console.log("Не высокосный год");
+ } else {
+	 console.log("Высокосный год");
+ }
+ 
+//Задача №6
+
+let plates = prompt("Количество тарелок", 10);
+let gel = prompt("Количество моющего средства", 10);
+const COEFF = 0.5;
+
+while ( plates > 0 && gel > 0 ) {
+	plates--;
+	gel -= 0.5;
+}
+
+console.log( "Количество непомытых тарелок: " + plates + " " + "Количество оставшегося моющего средства: " + gel );
